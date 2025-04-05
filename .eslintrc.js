54f0ef6a27fdb39b5193b01aed4a68fdf8377ccc
laurenr3-web <lau_delisle@hotmail.com>
2025-04-05 13:51:19 -0400

    Mise à jour de la configuration ESLint pour compatibilité avec ESLint v8

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react-refresh',
+    'unused-imports',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-call': 'error',
+    '@typescript-eslint/no-unsafe-return': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { 
+        vars: 'all', 
+        varsIgnorePattern: '^_', 
+        args: 'after-used', 
+        argsIgnorePattern: '^_' 
+      }
+    ]
+  },
+  ignorePatterns: ['dist']
+};
